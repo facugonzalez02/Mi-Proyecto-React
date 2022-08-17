32835a4567654bca89d392536c68a2af8fb2c034
facugonzalez02 <facugonzalez0743@gmail.com>
2022-08-17 17:06:54 -0300

    Desaparece Boton Vaciar

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,6 @@
+import { Card } from "react-bootstrap"
+import CardHeader from 'react-bootstrap/esm/CardHeader'
+import { Link } from "react-router-dom"
 import { useCartContext } from '../../Context/CartContext';
 
 const Cart = () => {
@@ -5,28 +8,41 @@ const Cart = () => {
   const{cartList, vaciarCarrito, eliminarProducto, precioTotal} = useCartContext ()
   
   return (
-    <div className="row">
-      <h1>Carrito</h1>
-        <div className="col-8">
-            <div className="w-100">                    
-                    {cartList.map(item => (
-                        <div key={item.id}>
+    <>
+        {cartList.length == 0 ? 
+
+                
+
+            <Card className="text-center mx-auto" style={{ width: '10rem', marginTop: '20px' }}>
+            <CardHeader>Aun no tienes nada en tu carrito</CardHeader>
+            <button><Link to="/" style={{  color: "black" }}>Ir a comprar</Link></button>
+            </Card>
+            
+            :
+        <div className="row">
+        <h1>Carrito</h1>
+            <div className="col-8">
+                <div className="w-100">                    
+                        {cartList.map(item => (
+                            <div key={item.id}>
+                                
+                                <img src={item.foto} alt="Foto de producto" style={{ width: 100 }} />
                             
-                            <img src={item.foto} alt="Foto de producto" style={{ width: 100 }} />
-                          
-                            nombre: {item.categoria} - cantidad: {item.cantidad} - Precio: {item.precio} - Subtotal: {item.cantidad * item.precio}
-                            <button onClick={ () => eliminarProducto(item.id) }> X </button>
-                        </div>
-                    ))}
-                
-            </div>
-            <div>
-                <h6>  { precioTotal() !== 0 && `Precio Total: ${ precioTotal() }`}</h6>
-                
-                <button onClick={vaciarCarrito}>Vaciar carrito</button>
+                                nombre: {item.categoria} - cantidad: {item.cantidad} - Precio: {item.precio} - Subtotal: {item.cantidad * item.precio}
+                                <button onClick={ () => eliminarProducto(item.id) }> X </button>
+                            </div>
+                        ))}
+                    
+                </div>
+                <div>
+                    <h6>  { precioTotal() !== 0 && `Precio Total: ${ precioTotal() }`}</h6>
+                    
+                    <button  onClick={vaciarCarrito}>Vaciar carrito</button>
+                </div>
             </div>
         </div>
-    </div>
+}
+    </>
 )
 }
   

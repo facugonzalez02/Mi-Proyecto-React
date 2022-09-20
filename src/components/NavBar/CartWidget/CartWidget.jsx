c4dd39fc388552fbe36c50ae6b168e8182e96d27
facugonzalez02 <facugonzalez0743@gmail.com>
2022-09-20 19:16:52 -0300

    imagen carrito

--- src/components/NavBar/CartWidget/CartWidget.jsx
+++ src/components/NavBar/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ const estiloWidget= {marginLeft:"15px", height:"42px", widht: "42px"}
 const CartWidget = () => {
   return (
     <div style={estiloWidget}>
-      <img src='/Imagenes/carrito.png' alt="Carrito" style={estiloWidget}/>
+      <img src='https://drive.google.com/uc?export=download&id=130AagOi1DG_8OSYA-jOsE9F5SGpbhBZc' alt="Carrito" style={estiloWidget}/>
     </div>
   )
 }
